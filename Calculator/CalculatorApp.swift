2b7a47af77642e947ff03ce0e0a80da46c448de9
dongmin9117 <ehdals5108@gmail.com>
2021-08-30 20:26:37 +0900

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Walter yun on 2021/08/30.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
